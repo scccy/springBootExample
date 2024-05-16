3f4d03b03c8147ee63a18a205514843c6ea7b087
scccy <shici2880@gmail.com>
2024-05-16 22:24:21 +0800

    首次提交

--- src/main/java/com/scccy/springbootexample01/handlerExption/GlobalExceptionHandler.java
+++ src/main/java/com/scccy/springbootexample01/handlerExption/GlobalExceptionHandler.java
@@ -3,12 +3,14 @@ package com.scccy.springbootexample01.handlerExption;
 
 import com.scccy.springbootexample01.common.ResultData;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.datasource.DataSourceException;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
 
 @Slf4j
@@ -16,7 +18,7 @@ import java.sql.SQLIntegrityConstraintViolationException;
 public class GlobalExceptionHandler {
     //    针对数据库异常处理
     @ExceptionHandler(DataAccessException.class)
-    public ResponseEntity<ResultData> handleSQLException(DataAccessException ex) {
+    public ResponseEntity<ResultData> handleSQLException(SQLException ex) {
         String errorMessage = "Database error: SQL integrity constraint violation.";
         log.error(errorMessage, ex);
         return new ResponseEntity<>(ResultData.fail().setMsg(errorMessage).setData(ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
